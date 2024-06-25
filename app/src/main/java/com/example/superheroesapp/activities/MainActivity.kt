c9cc79025ec8ec71ada06133cf5fccd5e92c17a9
raulfl1982ndrd <raulfl1982ndrd@gmail.com>
2024-06-25 10:36:45 +0200

    Se implementa el Search con el menu en el en el action bar

--- app/src/main/java/com/example/superheroesapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/superheroesapp/activities/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.superheroesapp.activities
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.superheroesapp.R
 import com.example.superheroesapp.adapters.SuperheroAdapter
 import com.example.superheroesapp.data.SuperheroApiService
+import com.example.superheroesapp.data.SuperheroResponse
 import com.example.superheroesapp.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -17,24 +23,64 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var adapter: SuperheroAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        adapter = SuperheroAdapter()
+        adapter = SuperheroAdapter ()
+
         binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = GridLayoutManager(this, 2)
+        /*binding.searchButton.setOnClickListener {
+            val searchText = binding.searchEditText.text.toString()
+            searchSuperheroes(searchText)
+        }*/
+        //searchByName("a")
+        binding.progress.visibility = View.GONE
+        binding.recyclerView.visibility = View.GONE
+        binding.emptyPlaceholder.visibility = View.VISIBLE
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main_menu, menu)
 
-        searchByName("a")
+        initSearchView(menu?.findItem(R.id.menu_search))
+
+        return true
+    }
+
+    private fun initSearchView(searchItem: MenuItem?) {
+        if (searchItem != null) {
+            var searchView = searchItem.actionView as SearchView
+
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    //searchSuperheroes(query!!)
+                    searchByName(query!!)
+                    searchView.clearFocus()
+                    return true
+                }
+
+                override fun onQueryTextChange(query: String?): Boolean {
+                    return false
+                }
+            })
+        }
     }
 
     private fun searchByName(query: String){
+        binding.progress.visibility = View.VISIBLE
         // Llamada en segundo plano
         CoroutineScope(Dispatchers.IO).launch {
             try {
                 val apiService = getRetrofit().create(SuperheroApiService::class.java)
                 val result = apiService.findSuperheroesByName(query)
                 runOnUiThread {
+                    binding.progress.visibility = View.GONE
+                    binding.recyclerView.visibility = View.VISIBLE
+                    binding.emptyPlaceholder.visibility = View.GONE
                     adapter.updateData(result.results)
                 }
                 //Log.i("HTTP", "${result.results}")
